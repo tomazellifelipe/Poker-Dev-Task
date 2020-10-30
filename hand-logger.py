e58fa84c2051bfcd86af63e8d17634b857071963
Felipe Tomazelli <fefetl08@gmail.com>
2020-10-30 15:12:35 -0300

    refactor: nome de métodos

--- hand-logger.py
+++ hand-logger.py
@@ -24,7 +24,7 @@ class Hand:
     def buttonPlace(self):
         self.button = int(re.search(r'[#][\d]+', self.log[1]).group()[1:])
 
-    def createplayers(self):
+    def findPlayersOnHand(self):
         i = 2
         while self.log[i][:4] == "Seat":
             self.players.append(Player(self.log[i]))
@@ -56,29 +56,30 @@ class Player():
         return f'\t- {self.name}, {self.stack}, {self.seat}\n'
 
 
-def openFile(filePath: str) -> list:
+def openAndSplitFile(filePath: str) -> list:
     with open(filePath) as file:
         return file.read().strip().split("\n\n")
 
 
-def splitLinesAndClean(text: str) -> list:
-    return [line.strip() for line in text.splitlines()]
+def splitIntoLines(text: str) -> list:
+    return [line.strip() for line in text.strip().splitlines()]
 
 
 filePath = "hand-samples\\Aaltje II-0.50-1-USD-NoLimitHoldem" \
            "-PokerStars-8-6-2017.txt"
 table = []
-handsLog = openFile(filePath)
-for log in handsLog:
-    hand = Hand(splitLinesAndClean(log.strip()))
-    hand.createplayers()
+handsData = openAndSplitFile(filePath)
+for handLog in handsData:
+    logByLine = splitIntoLines(handLog)
+    hand = Hand(logByLine)
+    hand.findPlayersOnHand()
     table.append(hand)
 
-f = open('hand-logs\\sampleLog.txt', 'a')
-for hand in table:
-    f.write(f'{hand}')
+f = open('hand-logs\\sampleLog.txt', 'w')
+for ihand in table:
+    f.write(f'{ihand}')
     f.write('Players names, stacks, seats\n')
-    for player in hand.players:
+    for player in ihand.players:
         f.write(f'{player}')
     f.write('\n')
 f.close()
